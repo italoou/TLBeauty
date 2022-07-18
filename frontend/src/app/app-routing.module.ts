b4d8161678ea7a874f932943efdb808da9059e9b
italo lima <italo.itlima@gmail.com>
2022-07-18 01:48:04 -0300

    adicionando rota cliente-detail

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientesComponent } from './clientes/clientes.component';
 import { LoginComponent } from './login/login.component';
+import { ClienteDetailComponent } from './cliente-detail/cliente-detail.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
-  {path: 'clientes', component: ClientesComponent}
+  {path: 'clientes', component: ClientesComponent},
+  {path: 'clientes/:id', component: ClienteDetailComponent}
 ];
 
 @NgModule({
